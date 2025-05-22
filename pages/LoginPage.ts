e573b8d50d33c4cc322cfbed78e60db9fe3cbae8
Ahmad Irshaid <145376048+AhmadIrsahaid@users.noreply.github.com>
2025-05-22 22:57:11 +0300

    first commit

---
+++ pages/LoginPage.ts
@@ -0,0 +1,23 @@
+import { Page } from '@playwright/test';
+import 'dotenv/config';
+export class LoginPage {
+  private usernameInput;
+  private passwordInput;
+  private loginButton;
+
+  constructor(private page: Page) {
+    this.usernameInput = page.locator('[data-test="username"]');
+    this.passwordInput = page.locator('[data-test="password"]');
+    this.loginButton = page.locator('[data-test="login-button"]');
+  }
+
+  async goto() {
+    await this.page.goto(process.env.BASE_URL!);
+  }
+
+  async login(username: string, password: string) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+}
---
+++ tests/login.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+import 'dotenv/config'
+import { LoginPage } from '../pages/LoginPage';
+
+test('Upon successful login, user is redirected to the inventory page', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login(process.env.USER_NAME!, process.env.PASSWORD!);
+  await expect(page).toHaveURL('inventory');
+});
+
+test('Attempt to sign in using a wrong username', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login('invalid_user', process.env.PASSWORD!);
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
+
+test('Attempt to sign in using a wrong password', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login(process.env.USER_NAME!, 'wrong');
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
+
+test('Attempt to log in with blank username and password fields', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login('', '');
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+})
