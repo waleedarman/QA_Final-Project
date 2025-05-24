6c9008428c18bbad50c23fd9158d26963128da33
Ahmad Irshaid <145376048+AhmadIrsahaid@users.noreply.github.com>
2025-05-24 19:50:54 +0300

    fix error in login test code

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,11 @@
 import { test, expect } from '@playwright/test';
 import 'dotenv/config'
 import { LoginPage } from '../pages/LoginPage';
-
 test('Upon successful login, user is redirected to the inventory page', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.goto();
   await loginPage.login(process.env.USER_NAME!, process.env.PASSWORD!);
-  await expect(page).toHaveURL('inventory');
+  await expect(page).toHaveURL("/inventory.html");
 });
 
 test('Attempt to sign in using a wrong username', async ({ page }) => {
